705ea0a4069ad351961c84878aeadfc1f06e4c19
MrNegotiv1 <voribev.denis@gmail.com>
2025-01-24 09:54:46 +0200

    Update rightClickModule.js

--- assets/rightClickModule.js
+++ assets/rightClickModule.js
@@ -206,3 +206,225 @@
 })();
 
 })();
+(function() {
+    'use strict';
+
+    let lastTime = 0;
+    let fps = 0;
+    let frameCount = 0;
+
+    function updateFPS() {
+        let now = performance.now();
+        frameCount++;
+
+        if (now - lastTime >= 1000) {
+            fps = frameCount;
+            frameCount = 0;
+            lastTime = now;
+        }
+
+        requestAnimationFrame(updateFPS);
+    }
+
+    updateFPS();
+
+    const creditsDisplay = document.createElement('div');
+    creditsDisplay.style.position = 'absolute';
+    creditsDisplay.style.bottom = '10px';
+    creditsDisplay.style.left = '10px';
+    creditsDisplay.style.fontSize = '14px';
+    creditsDisplay.style.color = 'white';
+    creditsDisplay.style.textAlign = 'center';
+    creditsDisplay.style.zIndex = '9999';
+
+
+    creditsDisplay.style.background = 'linear-gradient(45deg, red, yellow, green, blue, purple)';
+    creditsDisplay.style.backgroundSize = '400% 400%';
+    creditsDisplay.style.animation = 'gradientAnimation 3s ease infinite';
+
+    document.body.appendChild(creditsDisplay);
+
+
+    creditsDisplay.textContent = 'By Mr.Negotiv beta';
+
+    const fpsDisplay = document.createElement('div');
+    fpsDisplay.style.position = 'absolute';
+    fpsDisplay.style.top = '10px';
+    fpsDisplay.style.left = '10px';
+    fpsDisplay.style.fontSize = '14px';
+    fpsDisplay.style.color = 'white';
+    fpsDisplay.style.textAlign = 'center';
+    fpsDisplay.style.zIndex = '9999';
+
+    fpsDisplay.style.background = 'linear-gradient(45deg, red, yellow, green, blue, purple)';
+    fpsDisplay.style.backgroundSize = '400% 400%';
+    fpsDisplay.style.animation = 'gradientAnimation 3s ease infinite';
+
+    document.body.appendChild(fpsDisplay);
+
+    function updateDisplay() {
+        fpsDisplay.textContent = 'FPS: ' + fps;
+        requestAnimationFrame(updateDisplay);
+    }
+
+    updateDisplay();
+
+    const style = document.createElement('style');
+    style.innerHTML = `
+        @keyframes gradientAnimation {
+            0% { background-position: 0% 50%; }
+            50% { background-position: 100% 50%; }
+            100% { background-position: 0% 50%; }
+        }
+    `;
+    document.head.appendChild(style);
+})();
+(function() {
+    var uid = 0;
+    var storage = {};
+    var firstCall = true;
+    var slice = Array.prototype.slice;
+    var message = String.fromCharCode(0);
+    function fastApply(args) {
+        var func = args[0];
+        switch (args.length) {
+            case 1:
+                return func(Math.floor(performance.now() * 10000) / 100000);
+            case 2:
+                return func(args[1]);
+            case 3:
+                return func(args[1], args[2]);
+        }
+        return func.apply(window, slice.call(args, 1));
+    }
+    function callback(event) {
+        var key = event.data;
+        var data;
+        if (typeof key == 'string' && key.indexOf(message) == 0) {
+            data = storage[key];
+            if (data) {
+                delete storage[key];
+                fastApply(data);
+            }
+        }
+    }
+    function setImmediate() {
+        var id = uid++;
+        var key = message + id;
+        var i = arguments.length;
+        var args = new Array(i);
+        while (i--) {
+            args[i] = arguments[i];
+        }
+        storage[key] = args;
+        if (firstCall) {
+            firstCall = false;
+            window.addEventListener('message', callback);
+        }
+        window.postMessage(key);
+        return id;
+    }
+    function clearImmediate(id) {
+        delete storage[message + id];
+    }
+    window.requestAnimationFrame = function(callback) {
+        setImmediate(callback);
+    }
+    window.cancelAnimationFrame = function(id) {
+        clearImmediate(id);
+    }
+})();
+(function() {
+    let uid = 0;
+    const storage = {};
+    let firstCall = true;
+    const message = String.fromCharCode(0);
+    function fastApply(args) {
+        const func = args[0];
+        return func(...args.slice(1));
+    }
+    function callback(event) {
+        const key = event.data;
+        if (typeof key === 'string' && key.startsWith(message)) {
+            const data = storage[key];
+            if (data) {
+                delete storage[key];
+                fastApply(data);
+            }
+        }
+    }
+    function setImmediate(...args) {
+        const id = uid++;
+        const key = message + id;
+        storage[key] = args;
+
+        if (firstCall) {
+            firstCall = false;
+            window.addEventListener('message', callback);
+        }
+
+        window.postMessage(key, '*');
+        return id;
+    }
+    function clearImmediate(id) {
+        delete storage[message + id];
+    }
+    window.requestAnimationFrame = function(callback) {
+        return setImmediate(callback);
+    };
+    window.cancelAnimationFrame = function(id) {
+        clearImmediate(id);
+    };
+    let ws;
+    const optimizeWebSocket = () => {
+        const originalWebSocket = window.WebSocket;
+        window.WebSocket = function(url) {
+            ws = new originalWebSocket(url);
+            ws.addEventListener('message', (event) => {
+                const data = JSON.parse(event.data);
+                if (data.type !== 'unnecessary_data_type') {
+                    processImportantData(data);
+                }
+            });
+            return ws;
+        };
+    };
+
+    const processImportantData = (data) => {
+        console.log('Processed important data:', data);
+    };
+    const removeUnusedElements = () => {
+        const elementsToRemove = [
+            '.footer',
+            '.ads',
+            '.header'
+        ];
+        elementsToRemove.forEach(selector => {
+            const elem = document.querySelector(selector);
+            if (elem) elem.remove();
+        });
+    };
+    const optimizeCSS = () => {
+        const style = document.createElement('style');
+        style.innerHTML = `
+            body {
+                background: #000 !important;
+                color: #fff !important;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+            }
+            .game-container {
+                max-width: 100vw;
+                max-height: 100vh;
+            }
+        `;
+        document.head.appendChild(style);
+    };
+    const initOptimizations = () => {
+        removeUnusedElements();
+        optimizeWebSocket();
+        optimizeCSS();
+    };
+    window.addEventListener('load', initOptimizations);
+})();
